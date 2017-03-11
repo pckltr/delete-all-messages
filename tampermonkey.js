9d8f57dd79ef770702982f8cf743d4fed01eac11
Vlad <vlad.rusanescu@gmail.com>
2017-03-11 10:40:01 +0200

    Update tampermonkey.js

--- tampermonkey.js
+++ tampermonkey.js
@@ -1,34 +1,47 @@
 // ==UserScript==
 // @name         Delete All
-// @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Delete all messages from LinkedIn
 // @author       You
-// @match        *://*.linkedin.com/*
+// @match        *://*.linkedin.com/messaging*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
+    var deleteAllButton,
+        deleteAllButtonClass,
+        style;
+    
     var deleteAll = function() {
+        // get all delete buttons
         var deleteButtons = document.querySelectorAll('[data-control-name="clear_conversation"]'),
+        // get message list container
+            messageListContainer = document.getElementsByClassName('msg-conversations-container__conversations-list')[0],
             confirmButton;
-        console.log('delete all click');
+        //do {
         for (var i = 0; i < deleteButtons.length; i++) {
-            console.log('every click');
-            //deleteButtons.click();
-            //confirmButton = document.querySelectorAll('[data-control-name="clear_conversation_confirm"]');
-            //confirmButton.click();
+            deleteButtons[i].click();
+            setTimout(function() {
+                confirmButton = document.querySelectorAll('[data-control-name="clear_conversation_confirm"]');
+            },100);
+            confirmButton[0].click();
         }
-        document.getElementsByClassname('msg-conversations-container__conversations-list')[0].scrollTop = messageList.Height;
+        messageListContainer.scrollTop = messageListContainer.Height;
+        messageListContainer = document.getElementsByClassName('msg-conversations-container__conversations-list')[0];
+        //} while (messageListContainer.children.length > 0);
     };
-    var deleteAllButton = document.createElement('button');
-    var deleteAllButtonClass = 'delete-all-button';
+    
+    // create button element, set class and add to DOM
+    deleteAllButton = document.createElement('button');
+    deleteAllButtonClass = 'delete-all-button';
     deleteAllButton.innerHTML = 'Delete All Messages';
     deleteAllButton.className = deleteAllButtonClass;
-    deleteAllButton.addEventListener('click', deleteAll);
+    deleteAllButton.addEventListener('click', deleteAll);    
     document.getElementsByTagName("body")[0].appendChild(deleteAllButton);
-    var style = document.createElement('style');
+    
+    // create style element and add to DOM
+    style = document.createElement('style');
     style.innerHTML = '.' + deleteAllButtonClass + ' {position: fixed; top: 1em; right: 1em; z-index: 99999; color: white; background-color: #FCA61C; padding: 10px 20px; border: none; text-transform: uppercase; border-radius: 3em; font-family: sans-serif; font-weight: bold;}';
     document.getElementsByTagName("head")[0].appendChild(style);
 })();
